3d7ae9c1ced05caf1ef0360dd58d32c26251bf15
maaaaz <maaaaz@androwarn.com>
2015-06-07 17:28:07 -0400

    bugs fixed and exemple added

--- JavaSerializeXML.py
+++ JavaSerializeXML.py
@@ -7,7 +7,8 @@ from burp import IHttpRequestResponse
 import java.io
 import java.util.zip
 import org.python.util
-
+import sys
+import pprint
 import zlib
 import java.io.ByteArrayOutputStream
 import java.io.ByteArrayInputStream
@@ -18,128 +19,144 @@ import org.python.util.PythonObjectInputStream
 import java.lang.RuntimeException
 
 # import external Jars
-import sys
 from os import listdir
 from os.path import join
 
 extends_jars = lambda path: [join(path, jar) for jar in listdir(path) if jar.endswith(".jar")]
 sys.path.extend(extends_jars(sys.path[-1]))
 
-
 import com.thoughtworks.xstream.XStream
 import com.thoughtworks.xstream.io.xml.DomDriver
 
 class BurpExtender(IBurpExtender, IMessageEditorTabFactory):
-
-    #
-    # implement IBurpExtender
-    #
-    def registerExtenderCallbacks(self, callbacks):
-
-        # keep a reference to our callbacks object
-        self._callbacks = callbacks
-
-        # obtain an extension helpers object
-        self._helpers = callbacks.getHelpers()
-
-        # set our extension name
-        callbacks.setExtensionName("Java Serialized XML viewer")
-
-        # register ourselves as a message editor tab factory
-        callbacks.registerMessageEditorTabFactory(self)
-
-        return
-
-    #
-    # implement IMessageEditorTabFactory
-    #
-    def createNewInstance(self, controller, editable):
-
-        # create a new instance of our custom editor tab
-        return JavaSerializeXMLInputTab(self, controller, editable)
-
-#
+	
+	#
+	# implement IBurpExtender
+	#
+	
+	def	registerExtenderCallbacks(self, callbacks):
+	
+		# keep a reference to our callbacks object
+		self._callbacks = callbacks
+		
+		# obtain an extension helpers object
+		self._helpers = callbacks.getHelpers()
+		
+		# set our extension name
+		callbacks.setExtensionName("Java serialized-to-XML editor")
+		
+		# register ourselves as a message editor tab factory
+		callbacks.registerMessageEditorTabFactory(self)
+		
+		return
+		
+	# 
+	# implement IMessageEditorTabFactory
+	#
+	
+	def createNewInstance(self, controller, editable):
+		
+		# create a new instance of our custom editor tab
+		return JavaSerializeXMLInputTab(self, controller, editable)
+		
+# 
 # class implementing IMessageEditorTab
 #
 class JavaSerializeXMLInputTab(IMessageEditorTab):
 
-    def __init__(self, extender, controller, editable):
-        self._extender = extender
-        self._editable = editable
+	def __init__(self, extender, controller, editable):
+		self._extender = extender
+		self._editable = editable
+		
+		
+		# create an instance of Burp's text editor, to display our deserialized data
+		self._txtInput = extender._callbacks.createTextEditor()
+		self._txtInput.setEditable(False)
+		return
+		
+	#
+	# implement IMessageEditorTab
+	#
+	def getTabCaption(self):
+		return "Java serialized-to-XML editor"
+		
+	def getUiComponent(self):
+		return self._txtInput.getComponent()
+		
+	def isEnabled(self, content, isRequest):
+		# enable this tab for requests containing a data parameter
+		return True
+		
+	def setMessage(self, content, isRequest):
+		#print "SET MESSAGE"
+		if (content is None):
+			print "content none"
+			# clear our display
+			self._txtInput.setText(None)
+			self._txtInput.setEditable(False)
+		
+		else:
+			# retrieve the data parameter
+			requestinfo = self._extender._helpers.analyzeRequest(content)
+				
+			bodyoffset = requestinfo.getBodyOffset()
+			
+			payload_serialized_deflate = content[bodyoffset:]
+			payload_serialized_inflate = zlib.decompress(payload_serialized_deflate)
+			
+			b = java.io.ByteArrayInputStream(payload_serialized_inflate)
+			
+			try:
+				i = org.python.util.PythonObjectInputStream(b).readObject()
+			except java.lang.RuntimeException, e:
+				self._txtInput.setText("Erreur lors de la tentative de deserialization du contenu")
+				self._currentMessage = content
+				return
+			
+			xstream = com.thoughtworks.xstream.XStream(com.thoughtworks.xstream.io.xml.DomDriver())
+			xml_deserialized = xstream.toXML(i)
 
-
-        # create an instance of Burp's text editor, to display our deserialized data
-        self._txtInput = extender._callbacks.createTextEditor()
-        self._txtInput.setEditable(False)
-        return
-
-    #
-    # implement IMessageEditorTab
-    #
-    def getTabCaption(self):
-        return "Java Serialized XML viewer"
-
-    def getUiComponent(self):
-        return self._txtInput.getComponent()
-
-    def isEnabled(self, content, isRequest):
-        # enable this tab for requests containing a data parameter
-        #return isRequest and not self._extender._helpers.getRequestParameter(content, "data") is None
-        return True
-
-    def setMessage(self, content, isRequest):
-        if (content is None):
-            print "content none"
-            # clear our display
-            self._txtInput.setText(None)
-            self._txtInput.setEditable(False)
-
-        else:
-            # retrieve the data parameter
-            request_info = self._extender._helpers.analyzeRequest(content)
-            bodyoffset = request_info.getBodyOffset()
-
-            payload_serialized_deflate = content[bodyoffset:]
-            payload_serialized_inflate = zlib.decompress(payload_serialized_deflate)
-
-            byte_array  = java.io.ByteArrayInputStream(payload_serialized_inflate)
-
-            try:
-                py_stream = org.python.util.PythonObjectInputStream(byte_array).readObject()
-            except java.lang.RuntimeException, e:
-                self._txtInput.setText("Erreur lors de la tentative de deserialization du contenu")
-                self._currentMessage = content
-                return
-
-            xstream = com.thoughtworks.xstream.XStream(com.thoughtworks.xstream.io.xml.DomDriver())
-            xml_deserialized = xstream.toXML(i)
-
-            # deserialize the parameter value
-            self._txtInput.setText(xml_deserialized)
-            self._txtInput.setEditable(False)
-
-        # remember the displayed content
-        self._currentMessage = content
-        return
-
-    def getMessage(self):
-        # determine whether the user modified the deserialized data
-        if (self._txtInput.isTextModified()):
-            # reserialize the data
-            text = self._txtInput.getText()
-            payload_serialized_deflate = zlib.compress(text)
-
-            new_request_body = payload_serialized_deflate
-
-            request_info = self._extender._helpers.analyzeRequest(self._currentMessage)
-
-            new_http_message = self._extender._helpers.buildHttpMessage(request_info.getHeaders(), new_request_body)
-            return new_http_message
-        else:
-            return self._currentMessage
-
-    def isModified(self):
-        return self._txtInput.isTextModified()
-
-    def getSelectedData(self):
-        return self._txtInput.getSelectedText()
+			# deserialize the parameter value
+			self._txtInput.setText(xml_deserialized)
+			self._txtInput.setEditable(True)
+		
+		# remember the displayed content
+		self._currentMessage = content
+		return
+	
+	def getMessage(self):
+		#print "GET MESSAGE"
+		# determine whether the user modified the deserialized data
+		if (self._txtInput.isTextModified()):
+			# reserialize the data
+			text = self._txtInput.getText()
+			
+			xstream = com.thoughtworks.xstream.XStream(com.thoughtworks.xstream.io.xml.DomDriver())
+			to_serialize = xstream.fromXML(text.tostring())
+			
+			bStream = java.io.ByteArrayOutputStream()
+			oStream = java.io.ObjectOutputStream(bStream)
+			oStream.writeObject(to_serialize)
+			o = bStream.toByteArray()
+			
+			payload_serialized_deflate = zlib.compress(o)
+			
+			input = payload_serialized_deflate
+			
+			content = self._currentMessage
+			requestinfo = self._extender._helpers.analyzeRequest(content)
+			
+			bodyoffset = requestinfo.getBodyOffset()
+		
+			r = self._extender._helpers.analyzeRequest(self._currentMessage)
+	
+			return self._extender._helpers.buildHttpMessage(r.getHeaders(), input)
+			
+		else:
+			return self._currentMessage
+	
+	def isModified(self):
+		return self._txtInput.isTextModified()
+	
+	def getSelectedData(self):
+		return self._txtInput.getSelectedText()
--- README.md
+++ README.md
@@ -1,18 +1,37 @@
-MISC : Introduction au développement d'extensions Burp, Exemple Java sérialization
+MISC : Introduction au développement d'extensions Burp - Exemple Java sérialization
 ==================================================================================
 
 Description
 -----------
-Ce dépôt contient les sources et binaires mentionnés dans l'article MISC.
- * La librairie Java `Jython` permet d'utiliser un script écrit en Python comme extension de Burp.
- * Le script `JavaSerializeXML.py` permet d'ajouter un onglet de visualisation des requêtes Java désérialisées au sein de Burp.
- * La librarie Java `xstream` permet de convertir une requête Java sérialisée en un fichier XML et inversement.
+Ce dépôt contient les sources et binaires mentionnés dans l'article "Introduction au développement d'extensions Burp" du MISC n°XX.
+Les ressources sont regroupées ainsi :
+```
++---JavaSerializeXML.py	: Le code Jython de l'extension
+|
++---Exemple				: Un exemple d'application Java échangeant des objets Java sérialisés-compressés en HTTP
+|   +---bin				: Les fichiers .class de l'application et le fichier jar de la classe Employee
+|   +---src				: Le fichiers sources des classes de l'application
+```
 
-Afin d'utiliser l'extension, il convient d'ajouter le chemin du dossier `jar` de ce repository dans le champ `Folder for loading modules (optional)` de l'onglet `Extender/Options` de Burp. Une fois l'extension lancée, un onglet `Java Serialized XML viewer` apparait lors de la visualisation (onglet Proxy) et de la modification (onglet Repeater) des requêtes au sein de Burp.
+
+Usage
+-----
+0. Sélectionner le chemin vers l'interpréteur Jython au sein de Burp ; 
+1. Sélectionner un dossier dans le champ `Folder for loading modules (optional)` de l'onglet `Extender/Options` de Burp ;
+2. Ajouter les fichiers JAR de la bibliothèques XStream et de la classe Java `Employee` dans le champ sélectionné précédemment ;
+3. Charger l'extension "JavaSerializeXML.py" au sein de l'onglet `Extender` : un onglet `Java serialized-to-XML editor` apparait lors de la visualisation (onglet Proxy) et de la modification (onglet Repeater) des requêtes au sein de Burp.
+
+
+Dépendances
+-----------
+* La bibliothèque `Jython`, téléchargeable [ici](http://search.maven.org/remotecontent?filepath=org/python/jython-standalone/2.7.0/jython-standalone-2.7.0.jar)
+* La bibliothèque `XStream` [ici](https://nexus.codehaus.org/content/repositories/releases/com/thoughtworks/xstream/xstream/1.4.8/xstream-1.4.8.jar), qui assure la conversion objet objet Java sérialisé-compressé en XML et inversement
+
 
 Copyright et licence
 ---------------------
 Toutes les ressources de ce dépôt sont distribuées sous licence GPLv3.
+
 
 Crédits
 -------
